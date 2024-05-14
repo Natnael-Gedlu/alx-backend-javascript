734fc45db9d46e041e1c5cbe5a6ffab4764835eb
Natnael-Gedlu <nategd800@gmail.com>
2024-05-14 13:43:37 +0300

    Node_JS_basic

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+/**
+ * Creates an HTTP server that responds with "Hello Holberton School!".
+ */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
