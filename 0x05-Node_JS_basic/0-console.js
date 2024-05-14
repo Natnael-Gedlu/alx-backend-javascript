cc52532b7708cd5c7a5eeb13e63e72d416b9a2b9
Natnael-Gedlu <nategd800@gmail.com>
2024-05-14 13:35:41 +0300

    Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+/**
+ * displayMessage function displays a message to the console.
+ *
+ * @param {string} str - The message to be displayed.
+ */
+const displayMessage = (str) => console.log(str);
+
+module.exports = displayMessage;
